d5e92c9b63a760d4d88b3b04c9830b977094bb37
Abhiroybossy <120171394+Abhiroybossy@users.noreply.github.com>
2023-02-09 10:16:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5233742848").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Abhiroybossy/MUSIC-BOTUTUT")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AellyOfficial/AnonXMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
